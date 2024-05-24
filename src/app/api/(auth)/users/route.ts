6f935850bcecd4021122b0744b29ff7e56a32127
Nur E Azam (Nur) <91000171+nureazambd@users.noreply.github.com>
2024-05-25 00:32:02 +0600

    post api created

--- src/app/api/(auth)/users/route.ts
+++ src/app/api/(auth)/users/route.ts
@@ -10,3 +10,22 @@ export const GET = async () =>{
         return new NextResponse("Error in fetching users" + error, {status: 500})
     }
 };
+
+export const POST = async (request: Request) =>{
+    try {
+        const body = await request.json();
+        await connect();
+        const newUser = new User(body);
+        await newUser.save();
+
+        return new NextResponse(JSON.stringify({message: "User is created", user: newUser}), {status: 201});
+    } catch (error) {
+        return new NextResponse(
+            JSON.stringify({
+                message: "Error in creating user",
+                error
+            })
+        )
+    
+    }
+}
