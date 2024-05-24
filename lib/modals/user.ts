0df06b3ac0ff0206d0501579d1a121c67cf9745e
Nur E Azam (Nur) <91000171+nureazambd@users.noreply.github.com>
2024-05-24 14:52:31 +0600

    note schema done

--- lib/modals/user.ts
+++ lib/modals/user.ts
@@ -3,8 +3,8 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
     email: {type: String, required: true, unique: true},
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-
+    password: {type: String, required: true}
+    
 });
 
 const User = models.User || model("User", UserSchema);
