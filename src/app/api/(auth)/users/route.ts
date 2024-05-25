458c1df733b5675bcee338c64f41882886009ac2
Nur E Azam (Nur) <91000171+nureazambd@users.noreply.github.com>
2024-05-25 11:24:03 +0600

    patch api created, user update successfully done

--- src/app/api/(auth)/users/route.ts
+++ src/app/api/(auth)/users/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from "next/server"
 import connect from "../../../../../lib/db";
 import User from "../../../../../lib/modals/user";
+import { Types } from "mongoose";
+
+const ObjectId = require("mongoose").Types.ObjectId;
+
 export const GET = async () =>{
     try{
         await connect();
@@ -27,5 +31,64 @@ export const POST = async (request: Request) =>{
             })
         )
     
+    }
+};
+
+export const PATCH = async (request: Request)=>{
+    try {
+        const body = await request.json();
+        const {userId, newUsername} = body;
+        await connect();
+        if(!userId || !newUsername){
+            return new NextResponse(
+                JSON.stringify({message: "Id or new username are required"}),
+                {
+                    status: 400
+                }
+            )
+        }
+
+        if(!Types.ObjectId.isValid(userId)){
+            return new NextResponse(JSON.stringify({message:"Invalid userId"}),
+        {
+            status:400
+        }
+        )
+        }
+
+        const updatedUser = await User.findOneAndUpdate(
+            // {_id: new ObjectId(userId)},
+            {_id: new ObjectId(userId)},
+            {username: newUsername},
+            {new: true}
+        );
+
+        if(!updatedUser){
+            return new NextResponse(JSON.stringify({message:"User not found or didn't update user successfully."}),
+            {
+                status:400
+            }
+            )
+        }
+
+        return new NextResponse(
+            JSON.stringify({
+                message: "Username updated successfully",
+                user: updatedUser
+            }),
+            {
+                status: 200
+            }
+        )
+    } catch (error) {
+        return new NextResponse(
+            JSON.stringify({
+                message: "Error updating username",
+                error
+            }),
+            {
+                status: 500
+            }
+        )
     }
 }
